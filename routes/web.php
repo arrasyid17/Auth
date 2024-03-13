2b78cd1d4d7022a6c3addcda109cb43b25647357
arrasyid17 <alhafiidar@gmail.com>
2024-03-13 15:01:24 +0700

    apa aja

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
-Route::get('/', function () {
+Route::get('/register', function () {
     return view('register');
 });
